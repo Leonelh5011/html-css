0db126b5b061eb688d34860f36f11793d81a0b6a
LauriMedusa <edlenoel@hotmail.com>
2019-07-20 16:34:04 -0500

    try java

---
+++ javas/script.js
@@ -0,0 +1,18 @@
+console.log('hola!')
+
+
+
+const fn = {
+    fn: myfn
+}
+const arraycito = ["ft", fn, false]
+
+
+
+function melo() {
+    return ["ft", {
+        fn: function myfn() {
+            return "lo hice"
+        }
+    }, false]
+}
